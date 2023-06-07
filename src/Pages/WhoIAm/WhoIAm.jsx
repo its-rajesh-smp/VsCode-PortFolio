eff8e2454266146f3b0efd2d58da05c029b53e1e
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-06-07 17:24:27 +0530

    Empty

--- src/Pages/WhoIAm/WhoIAm.jsx
+++ src/Pages/WhoIAm/WhoIAm.jsx
@@ -10,6 +10,7 @@ function WhoIAm(props) {
         <div className="WhoIAm-div__center__nameContainer">
           <h1 className="first">Hi</h1>
           <h1 className="second">Myself Rajesh</h1>
+          <p>I am a Frontend Developer</p>
         </div>
         <img id="myImg" src="src\assets\IMG20220726073737.jpg" alt="" />
       </div>
